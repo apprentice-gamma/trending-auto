91540206cb9b0cbf0b9cf3a42a5a254450cc2197
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2015-04-21 19:49:02 -0400

    Adds controller logic using codebird-js, but it is doing nothing and giving no error messages -- back to the drawing board

--- js/app.js
+++ js/app.js
@@ -1,7 +1,33 @@
 var app = angular.module('twitterApp', [
 	]);
-	
-	
+
+	app.controller('trendController', ['$scope', function($scope) {
+
+  			 $scope.getToken = function(){
+  			 	console.log("method gets called.");
+  			 	cb.__call(
+			    "oauth2_token",
+			    {},
+			    function (reply) {
+			        var bearer_token = reply.access_token;
+		    	});
+
+  			 return bearer_token;
+  			 console.log("token: "+bearer_token);
+  			 }
+		}]);
+
+
+
+	/*
+		cb.__call(
+		    "oauth2_token",
+		    {},
+		    function (reply) {
+		        var bearer_token = reply.access_token;
+		    }
+		);
+	*/
 
 
 
