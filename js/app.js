7d162aad9c4ec0240e79c7c7938946c8ddd6cdf3
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2015-04-22 10:33:44 -0400

    Makes twitter trends service so it can be injected. Should have made a new branch maybe.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 (function(){
 	var app = angular.module('twitterApp', [
-	'TwitterControllers'
+	'TwitterControllers',
+	'TwitterServices'
 	]);
 })();
 
---
+++ js/services.js
@@ -0,0 +1,35 @@
+(function() {	
+	
+	var TwitterServices = angular.module('TwitterServices', []);
+
+	TwitterServices.factory('Trends', [function(){
+			var vm = this;
+			var trends;
+      		//vm.hello = "Hello World!";
+			vm.getTrends = function(){
+			 	console.log("method gets called.");
+		        var cb = new Codebird;
+		        cb.setConsumerKey("yh8E0Xm5op6mwblzmeH4m9L2w","ksFe1fZkrQgI4npHgE8IEnfwXkPrF51uzvFcx8arUnIemEcNfd")
+		        cb.setBearerToken("AAAAAAAAAAAAAAAAAAAAAB9SfQAAAAAAdE5A7JO6%2FKTbDfrbLX5nJlz1fzU%3DyqULyVYgKi498Yz1cJxJxLVRsjOqHebi5znG6xPs9D9D8VzGKg");
+		        var params = { "id": 2391585};
+		        return cb.__call(
+		          "trends_place",
+		          params,
+		          function(reply){
+		            console.log(reply[0].trends); 
+		          }
+		        );
+        	/*
+			 	cb.__call(
+		    	"oauth2_token",
+		    	{},
+		    	function (reply) {
+              console.log(reply.access_token);
+	    		});
+        	*/
+        	trends = vm.getTrends();
+        	return trends;
+			}
+
+	}]);
+})();
