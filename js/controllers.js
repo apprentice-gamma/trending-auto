29aecf40d4dcbf315594a20cbadf7d81fee0f431
Kelsey Ando <kma3a@virginia.edu>
2015-04-22 09:50:39 -0400

    adding the stuff for getting things from the twitter api with placeholders so we don 't push sensitive things

--- js/controllers.js
+++ js/controllers.js
@@ -3,18 +3,29 @@
 	
 	TwitterControllers.controller('TrendController', [function(){
 			var vm = this;
+      vm.hello = "Hello World!";
 			vm.getToken = function(){
 			 	console.log("method gets called.");
-			 	/*
+        var cb = new Codebird;
+        cb.setConsumerKey("Consumerkey(API)","Secret")
+        cb.setBearerToken("BearerToken");
+        var params = { "id": 2391585};
+        return cb.__call(
+          "trends_place",
+          params,
+          function(reply){
+            console.log(reply[0].trends); 
+          },
+          true
+        );
+        /*
 			 	cb.__call(
 		    	"oauth2_token",
 		    	{},
 		    	function (reply) {
-		       		 var bearer_token = reply.access_token;
+              console.log(reply.access_token);
 	    		});
-			 return bearer_token;
-			 console.log("token: "+bearer_token);
-			 */
+        */
 			}
 		}]);
 })();
