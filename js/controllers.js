fdd7157b1f8a8ea5db439fdb534f995d777c95b3
Linda Swanson <linda.swanson@detroitlabs.com>
2015-04-22 18:56:34 -0400

    displaying top 10 twitter headlines vertically for now

--- js/controllers.js
+++ js/controllers.js
@@ -1,14 +1,20 @@
-(function() {	
+(function() {
 	var TwitterControllers = angular.module('TwitterControllers', [
 		'TwitterServices'
 		]);
-	
+
 	TwitterControllers.controller('TrendController', [
 		'Trends',
 		function(Trends){
 			var vm = this;
-      		vm.hello = "Hello World!";
+			vm.trendsNameArray = [];
 			vm.trends = Trends;
+			vm.trends.then(function(headlineArray){
+				headlineArray.forEach(function(singleTrend) {
+					console.log(singleTrend.name);
+					vm.trendsNameArray.push(singleTrend.name);
+				})
+			})
 	}]);
 
 	TwitterControllers.controller('SpeechController', [
