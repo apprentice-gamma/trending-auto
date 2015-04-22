5901f53e6f2e0f02e45a532e10f5b11347b52f9e
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2015-04-22 14:43:04 -0400

    Refactor of controller.

--- js/controllers.js
+++ js/controllers.js
@@ -1,31 +1,17 @@
 (function() {	
-	var TwitterControllers = angular.module('TwitterControllers', []);
+	var TwitterControllers = angular.module('TwitterControllers', [
+		'TwitterServices'
+		]);
 	
-	TwitterControllers.controller('TrendController', ["consumerKey","consumerSecret","bearerToken",function(consumerKey,consumerSecret,bearerToken ){
+	TwitterControllers.controller('TrendController', [
+		'Trends',
+		function(Trends){
 			var vm = this;
-      vm.hello = "Hello World!";
-			vm.getToken = function(){
-			 	console.log("method gets called.");
-        var cb = new Codebird;
-        cb.setConsumerKey(consumerKey,consumerSecret)
-        cb.setBearerToken(bearerToken);
-        var params = { "id": 2391585};
-        return cb.__call(
-          "trends_place",
-          params,
-          function(reply){
-            console.log(reply[0].trends); 
-          },
-          true
-        );
-        /*
-			 	cb.__call(
-		    	"oauth2_token",
-		    	{},
-		    	function (reply) {
-              console.log(reply.access_token);
-	    		});
-        */
-			}
-		}]);
+      		vm.hello = "Hello World!";
+			vm.trends = Trends;
+	}]);
+
+	TwitterControllers.controller('SpeechController', [
+		function(){
+	}]);
 })();
