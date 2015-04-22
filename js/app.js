5512f84eccf06f9f0e86731401031b8b7d351053
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2015-04-22 09:55:52 -0400

    Fix app.js conflict. I think I done goofed.

--- js/app.js
+++ js/app.js
@@ -1,25 +1,7 @@
-<<<<<<< HEAD
+
 (function(){
 	var app = angular.module('twitterApp', [
 	'TwitterControllers'
 	]);
 })();
 
-
-
-
-
-	/*
-		cb.__call(
-		    "oauth2_token",
-		    {},
-		    function (reply) {
-		        var bearer_token = reply.access_token;
-		    }
-		);
-	*/
-
-
-
-=======
->>>>>>> 0f605ed1f4408e3468dae7e818f380b710945a1a
