cbbf233ee0e3357d9f1271f553984d5b6910fdb6
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2015-04-22 10:13:57 -0400

    Whitespace fixes, trying to integrate speech and trends, speech will be in its own controller.

--- js/controllers.js
+++ js/controllers.js
@@ -3,29 +3,36 @@
 	
 	TwitterControllers.controller('TrendController', [function(){
 			var vm = this;
-      vm.hello = "Hello World!";
+      		vm.hello = "Hello World!";
 			vm.getToken = function(){
 			 	console.log("method gets called.");
-        var cb = new Codebird;
-        cb.setConsumerKey("Consumerkey(API)","Secret")
-        cb.setBearerToken("BearerToken");
-        var params = { "id": 2391585};
-        return cb.__call(
-          "trends_place",
-          params,
-          function(reply){
-            console.log(reply[0].trends); 
-          },
-          true
-        );
-        /*
+		        var cb = new Codebird;
+		        cb.setConsumerKey("yh8E0Xm5op6mwblzmeH4m9L2w","ksFe1fZkrQgI4npHgE8IEnfwXkPrF51uzvFcx8arUnIemEcNfd")
+		        cb.setBearerToken("AAAAAAAAAAAAAAAAAAAAAB9SfQAAAAAAdE5A7JO6%2FKTbDfrbLX5nJlz1fzU%3DyqULyVYgKi498Yz1cJxJxLVRsjOqHebi5znG6xPs9D9D8VzGKg");
+		        var params = { "id": 2391585};
+		        return cb.__call(
+		          "trends_place",
+		          params,
+		          function(reply){
+		            console.log(reply[0].trends); 
+		          },
+		          true
+		        );
+        	/*
 			 	cb.__call(
 		    	"oauth2_token",
 		    	{},
 		    	function (reply) {
               console.log(reply.access_token);
 	    		});
-        */
-			}
-		}]);
+        	*/
+		}
+	}]);
+
+	TwitterControllers.controller('SpeechController', [
+		function(){
+
+	}]);
+
+
 })();
