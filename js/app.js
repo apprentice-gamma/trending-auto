9b57f57feb2844233528478efd082a0f57ccee63
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2015-04-21 20:22:48 -0400

    Refactoring into IIFE

--- js/app.js
+++ js/app.js
@@ -2,20 +2,7 @@ var app = angular.module('twitterApp', [
 	'TrendController'
 	]);
 
-	app.controller('TrendController', ['$scope', function($scope) {
-
-		$scope.getToken = function(){
-		 	console.log("method gets called.");
-		 	/*cb.__call(
-	    	"oauth2_token",
-	    	{},
-	    	function (reply) {
-	       		 var bearer_token = reply.access_token;
-    		});
-		 return bearer_token;
-		 console.log("token: "+bearer_token);*/
-		}
-	}]);
+	
 
 
 
---
+++ js/trend.ctrl.js
@@ -0,0 +1,19 @@
+(function() {	
+	var trendController = angular.module('trendController', []);
+	
+
+			$scope.getToken = function(){
+			 	console.log("method gets called.");
+			 	/*
+			 	cb.__call(
+		    	"oauth2_token",
+		    	{},
+		    	function (reply) {
+		       		 var bearer_token = reply.access_token;
+	    		});
+			 return bearer_token;
+			 console.log("token: "+bearer_token);
+			 */
+			}
+		}]);
+})();
