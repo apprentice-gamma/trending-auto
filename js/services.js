29c0dd63f0b2e5f702b840d3034646261d7ac40e
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2015-04-23 10:04:16 -0400

    Adds skeleton for search service which is just a copy of the trends service for now.

--- js/services.js
+++ js/services.js
@@ -32,4 +32,35 @@
 			}
 			return vm.getTrends();
 	}]);
+
+	TwitterServices.factory('Search', [
+		"consumerKey",
+		"consumerSecret",
+		"bearerToken",
+		"$q",
+		function(consumerKey, consumerSecret, bearerToken, $q){
+			var vm = this;
+			var deferred = $q.defer();
+			vm.getTrends = function(){
+			 	console.log("method gets called.");
+		        var cb = new Codebird;
+		        cb.setConsumerKey(consumerKey, consumerSecret);
+		        cb.setBearerToken(bearerToken);
+		        var params = { "id": 2391585};
+		        
+		        var what = cb.__call(
+		          "trends_place",
+		          params,
+		          function(reply){
+		          	var data = reply[0].trends;
+		          	console.log(data);
+		            return deferred.resolve(data); 
+		          }
+		        );
+		        console.log(deferred.promise);
+				return deferred.promise;
+			}
+			return vm.getTrends();
+	}]);
+
 })();
