6a2dd56b316649d95d3f894f10b98c03283f7a41
Kelsey Ando <kma3a@virginia.edu>
2015-04-24 14:54:02 -0400

    adding changes so it will do national as well as local

--- js/controllers.js
+++ js/controllers.js
@@ -3,13 +3,15 @@
 		'TwitterServices'
 		]);
 
-	TwitterControllers.controller('TrendController', function(Trends,Speech, $scope, $interval){
+	TwitterControllers.controller('TrendController', function(Trends,Speech, $scope, $interval, $timeout){
     $interval(function() {
 			$scope.currentTrend = Speech.getTrend();
-      }, 100);
-			$scope.trends = Trends;
-			$scope.trends.then(function(responce){
+      }, 10);
+      $scope.runLocTrends = function() {
+			  $scope.trends = Trends.getTrends(2391585);;
+      	$scope.trends.then(function(responce){
         Speech.speak("Your trends for Detroit are");
+
         console.log("Test call once");
         responce.forEach(function(trend, index) {
           console.log("trend");
@@ -17,8 +19,35 @@
             Speech.speak(trendName);
           })
         })
-	});
 
+      };
+    $scope.runNatTrends = function() {
+			  $scope.trends = Trends.getTrends(23424977);;
+      	$scope.trends.then(function(responce){
+        Speech.speak("Your trends for the United States are");
+        console.log("Test call once");
+        responce.forEach(function(trend, index) {
+          console.log("trend");
+            trendName = trend.name;
+            Speech.speak(trendName);
+          })
+        })
+
+      };
+
+    $timeout(function() {
+      Speech.speak("Welcome to moTrend");
+    },10)
+    $timeout(function() {
+    $scope.runLocTrends();
+    },10)
+    $timeout(function() {
+      $scope.runNatTrends()
+    },10000)
+      
+
+	});
+/*
 	TwitterControllers.controller('SearchController',[
 		'Search',
 		function(Search){
@@ -32,4 +61,5 @@
 			var vm = this;
 		}
 	]);
+  */
 })();
